4f8707006e89fff60d3ee0059e22e30da7082d22
KoichiHirata <ko_hirata@mac.com>
2019-02-19 16:08:48 +0900

    Initial Commit

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by 平田 浩一 on 2019/02/19.
+//  Copyright © 2019 Hirata Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
